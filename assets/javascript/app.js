9ca3608a8f8e11d556a831d1726e392141a3070b
Hibi <correiaph13@gmail.com>
2019-06-04 18:57:50 -0500

    got images to move and go still on click, styling to the left so that they show up side by side

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 //q: string Search query term or phrase.GIPHY search will automatically look for exact matches to queries + AND match + OR match. Explicit AND + OR boolean clauses in search queries are not supported.
 
@@ -12,59 +12,76 @@ var q;
 
 //adds the buttons for the default shows 
 
-function buttonGenerator () {
+function buttonGenerator() {
 
-    for (i=0; i<showList.length; i++){
+  for (i = 0; i < showList.length; i++) {
 
-        var newDivShow = $("<button>").text(showList[i]);
-        newDivShow.addClass("btn btn-dark btn-md");
-        newDivShow.attr(`data-showName`, showList[i]);
-        $('#button-group').append(newDivShow);
-        // console.log(showList[i])
-       
-        
-        ; }
+    var newDivShow = $("<button>").text(showList[i]);
+    newDivShow.addClass("btn btn-dark btn-md");
+    newDivShow.attr(`data-showName`, showList[i]);
+    $('#button-group').append(newDivShow);
+    // console.log(showList[i])
+
+
+    ;
+  }
 
 };
 
-buttonGenerator ();
+buttonGenerator();
 
 //performs the API query when a button is pressed, still working on having each search be what is actually in the button text
-$("button").on("click", function() {
-      var limit = 10;
-      q = $(this).attr("data-showName");
-    console.log($(this).attr("data-showName"));
-      var rating = "g"
-      var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIkey}&q=${q}&limit=${limit}&offset=0&rating=${rating}&lang=en`;
-      
-//styling - need to make gifs appear next to each other
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-         
-          $(".gifs-section").empty();
+$("button").on("click", function () {
+  var limit = 10;
+  q = $(this).attr("data-showName");
+  console.log($(this).attr("data-showName"));
+  var rating = "g"
+  var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIkey}&q=${q}&limit=${limit}&offset=0&rating=${rating}&lang=en`;
 
-          var results = response.data;
+  //styling - need to make gifs appear next to each other
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-            for (var j = 0; j < results.length; j++) {
-  
-              var gifDiv = $("<div>");
-              var rating = results[j].rating;
-              var p = $("<p>").text("Rating: " + rating);
-              var showImage = $("<img>");
-              showImage.attr("src", results[j].images.fixed_height.url);
-             
-              gifDiv.append(p);
-              gifDiv.append(showImage);
-              $(".gifs-section").append(gifDiv);
-              
-            }
-          });
-          });
+    $(".gifs-section").empty();
 
-  //
+    var results = response.data;
+
+    for (var j = 0; j < results.length; j++) {
+
+      var gifDiv = $("<div>");
+      gifDiv.addClass("imgDiv");
+      var rating = results[j].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var showImage = $("<img>");
+      showImage.attr("src", results[j].images.fixed_height.url);
+
+      var showImageStill = $("<img>");
+      showImageStill.attr("src", results[j].images.fixed_height_still.url);
+
+      console.log(results[j].images);
+
+      gifDiv.append(p);
+      gifDiv.append(showImage);
+      gifDiv.append(showImageStill);
+      showImage.addClass("movingImage");
+      showImageStill.addClass("stillImage");
+
+      $(".gifs-section").prepend(gifDiv);
+
+    }
+
+  });
+
 });
-    
-     
+
+$(document).on("click", "img", function () {
+  $(this).siblings("img").show();
+  $(this).hide();
+});
+  //
+// });
+
+
 
